6f074aed3a02e4c4d16ccfe3b4caac69c5f98b06
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-06-05 09:42:14 -0500

    Update Lambda_CrearUsuario.py

--- lambdas/Lambda_CrearUsuario.py
+++ lambdas/Lambda_CrearUsuario.py
@@ -7,34 +7,30 @@ def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 # Función que maneja el registro de user y validación del password
+
 def lambda_handler(event, context):
     try:
-        # Obtener el email y el password
-        user_id = event.get('user_id')
-        password = event.get('password')
-        
-        # Verificar que el email y el password existen
+        body = json.loads(event['body'])
+        user_id = body.get('user_id')
+        password = body.get('password')
+
         if user_id and password:
-            # Hashea la contraseña antes de almacenarla
             hashed_password = hash_password(password)
-            # Conectar DynamoDB
             dynamodb = boto3.resource('dynamodb')
             t_usuarios = dynamodb.Table('t_usuarios')
-            # Almacena los datos del user en la tabla de usuarios en DynamoDB
             t_usuarios.put_item(
                 Item={
                     'user_id': user_id,
-                    'password': hashed_password,
+                    'password': hashed_password
                 }
             )
-            # Retornar un código de estado HTTP 200 (OK) y un mensaje de éxito
             mensaje = {
                 'message': 'User registered successfully',
                 'user_id': user_id
             }
             return {
                 'statusCode': 200,
-                'body': mensaje
+                'body': json.dumps(mensaje)
             }
         else:
             mensaje = {
@@ -42,16 +38,15 @@ def lambda_handler(event, context):
             }
             return {
                 'statusCode': 400,
-                'body': mensaje
+                'body': json.dumps(mensaje)
             }
 
     except Exception as e:
-        # Excepción y retornar un código de error HTTP 500
         print("Exception:", str(e))
         mensaje = {
             'error': str(e)
-        }        
+        }
         return {
             'statusCode': 500,
-            'body': mensaje
+            'body': json.dumps(mensaje)
         }
